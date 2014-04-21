6c89de7fee5a28abaad99c3fd4eb306dbd266012
Satoshi Amemiya <satoshi_amemiya@voyagegroup.com>
2014-04-21 07:58:12 +0000

    Support for GET request

--- lib/kusabana/rule.rb
+++ lib/kusabana/rule.rb
@@ -4,11 +4,15 @@ require 'oj'
 module Kusabana
   class Rule
     attr_reader :expired
-    def initialize(method, pattern, expired, &modifier)
+    def initialize(method, pattern, expired)
       @method = method
       @pattern = pattern
       @expired = expired
-      @modifier = modifier
+			if block_given?
+				@modifier = -> (query) { yield query }
+		  else
+				@modifier = -> (query) { query }
+		  end
     end
 
     def match(method, path)
@@ -18,8 +22,12 @@ module Kusabana
     end
 
     def modify(query)
-      modified = @modifier.call(Oj.load(query, mode: :compat))
-      [Oj.dump(modified, mode: :compat), "#{@method}::#{@path}::#{modified.hash}"]
+			if query
+				modified = @modifier.call(Oj.load(query, mode: :compat))
+				[Oj.dump(modified, mode: :compat), "#{@method}::#{@path}::#{modified.hash}"]
+		  else
+				[nil, "#{@method}::#{@path}"]
+			end
     end
   end
 end
