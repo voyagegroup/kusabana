0074816470fa1d84653a4e4596aabd69a6e17e55
Satoshi Amemiya <satoshi_amemiya@voyagegroup.com>
2014-05-09 17:03:57 +0900

    Fix name of wrong variable

--- lib/kusabana/connection.rb
+++ lib/kusabana/connection.rb
@@ -10,6 +10,7 @@ module Kusabana
 
       @on_data = on_data
       @on_response = on_response
+      @on_finish = on_finish
 
       @req_parser = HTTP::Parser.new
       @req_buffer = ''
@@ -47,11 +48,11 @@ module Kusabana
         res_parser = HTTP::Parser.new
         res_parser.on_message_complete = on_parse_response(session_name)
         res_buffer = ''
-        @sessions[session_name] = {start: Time.new, res_parser: res_parser, res_buffer: res_buffer}
+        @sessions[session_name] = {start: Time.new}
 
         body, match = -> do
           @rules.each do |rule|
-            if rule.match(@req_parser.http_method,@req_parser.request_url)
+            if rule.match(@req_parser.http_method, @req_parser.request_url)
               @sessions[session_name].merge!(rule: rule, cache: true)
               return [rule.modify(@req_body), true]
             end
@@ -68,8 +69,8 @@ module Kusabana
           send_data res
           return
         end
-
-        @sessions[session_name][:cache_key] = cache_key
+        
+        @sessions[session_name].merge!(cache_key: cache_key, res_parser: res_parser, res_buffer: res_buffer, path: @req_parser.request_url, method: @req_parser.http_method)
         s = server session_name
         s.send_data ((match)? @req_buffer.gsub(/\r\n\r\n.+/m, "\r\n\r\n#{body}"): @req_buffer)
         @req_buffer.clear
@@ -82,7 +83,8 @@ module Kusabana
         caching = 'no'
         s = @sessions[session_name]
         log = {}
-        if s[:cache] && @req_parser.status_code == 200
+        s[:res_parser].status_code
+        if s[:cache] && s[:res_parser].status_code == 200
           store = @cache.set(s[:cache_key], s[:res_buffer], s[:rule].expired)
           if store
             caching = 'store'
@@ -91,7 +93,7 @@ module Kusabana
             caching = 'error'
           end
         end
-        @logger.res(log.merge(method: @req_parser.http_method, path: @req_parser.request_url, cache: caching, session: session_name, took: Time.new - s[:start], key: s[:cache_key]))
+        @logger.res(log.merge(method: s[:method], path: s[:path], cache: caching, session: session_name, took: Time.new - s[:start], key: s[:cache_key], status: s[:res_parser].status_code))
         send_data s[:res_buffer]
       end
     end
@@ -102,6 +104,12 @@ module Kusabana
         s[:res_buffer] << resp
         s[:res_parser] << resp
         nil
+      end
+    end
+
+    def on_finish
+      -> (backend) do
+        @sessions.delete(backend)
       end
     end
   end
