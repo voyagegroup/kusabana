7c98e59b9b3f31d71b64ec9ea0a6f3bb0e45f3b2
Satoshi Amemiya <satoshi_amemiya@voyagegroup.com>
2014-05-12 14:05:43 +0900

    Refactoring

--- lib/kusabana/connection.rb
+++ lib/kusabana/connection.rb
@@ -1,115 +1,34 @@
+# coding: UTF-8
 require 'uuid'
 
 module Kusabana
   class Connection < EM::ProxyServer::Connection
     def initialize(option)
-      @cache = option[:cache]
-      @logger = option[:logger]
-      @rules = option[:rules]
-      @es = option[:es]
-
+      @env = option[:env]
       @on_data = on_data
       @on_response = on_response
-      @on_finish = on_finish
-
-      @req_parser = HTTP::Parser.new
-      @req_buffer = ''
-      @req_body = ''
-      @req_parser.on_body = on_parse_request_body
-      @req_parser.on_message_complete = on_parse_request
-      @sessions = {}
-
-      super(option)
+      super
     end
 
     def server(session_name)
-      super session_name, :host => @es['host'], :port => @es['port']
+      super session_name, :host => @env.config['es']['remote']['host'], :port => @env.config['es']['remote']['port']
     end
 
     # Callbacks
     private
     def on_data
       ->(data) do
-        @req_buffer << data
-        @req_parser << data
+        req_parser = Kusabana::RequestParser.new(@env, self)
+        req_parser << data
         :async
-      end
-    end
-
-    def on_parse_request_body
-      ->(chunk) do
-        @req_body << chunk
-      end
-    end
-
-    def on_parse_request
-      -> do
-        session_name = UUID.generate :compact
-        res_parser = HTTP::Parser.new
-        res_parser.on_message_complete = on_parse_response(session_name)
-        res_buffer = ''
-        @sessions[session_name] = {start: Time.new}
-
-        body, match = -> do
-          @rules.each do |rule|
-            if rule.match(@req_parser.http_method, @req_parser.request_url)
-              @sessions[session_name].merge!(rule: rule, cache: true)
-              return [rule.modify(@req_body), true]
-            end
-          end
-          [@req_body, false]
-        end.call
-
-        cache_key = @req_parser.cache_key(body)
-        @logger.req(method: @req_parser.http_method, path: @req_parser.request_url, match: match, session: session_name, orig_query: @req_body, mod_query: body)
-        
-        if match && res = @cache.get_or_nil(cache_key)
-          @logger.res(method: @req_parser.http_method, path: @req_parser.request_url, cache: 'use', session: session_name, took: Time.new - @sessions[session_name][:start], key: cache_key)
-          @sessions.delete(session_name)
-          send_data res
-          return
-        end
-        
-        @sessions[session_name].merge!(cache_key: cache_key, res_parser: res_parser, res_buffer: res_buffer, path: @req_parser.request_url, method: @req_parser.http_method)
-        s = server session_name
-        s.send_data ((match)? @req_buffer.gsub(/\r\n\r\n.+/m, "\r\n\r\n#{body}"): @req_buffer)
-        @req_buffer.clear
-        @req_body.clear
-      end
-    end
-
-    def on_parse_response(session_name)
-      -> do
-        caching = 'no'
-        s = @sessions[session_name]
-        log = {}
-        s[:res_parser].status_code
-        if s[:cache] && s[:res_parser].status_code == 200
-          store = @cache.set(s[:cache_key], s[:res_buffer], s[:rule].expired)
-          if store
-            caching = 'store'
-            log[:expire] = s[:rule].expired
-          else
-            caching = 'error'
-          end
-        end
-        @logger.res(log.merge(method: s[:method], path: s[:path], cache: caching, session: session_name, took: Time.new - s[:start], key: s[:cache_key], status: s[:res_parser].status_code))
-        send_data s[:res_buffer]
       end
     end
 
     def on_response
       ->(backend, resp) do
-        s = @sessions[backend]
-        s[:res_buffer] << resp
+        s = @env.sessions[backend]
         s[:res_parser] << resp
-        nil
-      end
-    end
-
-    def on_finish
-      ->(backend) do
-        @sessions.delete(backend)
+        resp
       end
     end
   end
--- lib/kusabana/proxy.rb
+++ lib/kusabana/proxy.rb
@@ -7,30 +7,25 @@ require 'http/parser'
 module Kusabana
   class Proxy
     def initialize(rules, config)
-      @rules = rules
-      @config = config
-
-      @cache = Kusabana::Cache.new(@config['cache']['url'])
-      @logger = Kusabana::Logger.new(config['proxy']['output'] || STDOUT, 7, es: @config['es']['output'])
+      @env = Kusabana::Environment.new(rules, config)
     end
 
     def start
       begin
-        Process.daemon(true, true) if @config['proxy']['daemonize']
+        Process.daemon(true, true) if @env.config['proxy']['daemonize']
         EM.epoll
 
         EM.run do
           trap("TERM") { stop }
           trap("INT") { stop }
 
-          option = {cache: @cache, logger: @logger, rules: @rules, es: @config['es']['remote']}
-          EM::start_server(@config['proxy']['host'], @config['proxy']['port'], Kusabana::Connection, option)
-          EM::PeriodicTimer.new(300) { @logger.stat }
-          open(@config['proxy']['pid'] || 'kusabana.pid', 'w') {|f| f << Process.pid } if @config['proxy']['daemonize']
+          EM::start_server(@env.config['proxy']['host'], @env.config['proxy']['port'], Kusabana::Connection, env: @env)
+          EM::PeriodicTimer.new(300) { @env.logger.stat }
+          open(@env.config['proxy']['pid'] || 'kusabana.pid', 'w') {|f| f << Process.pid } if @env.config['proxy']['daemonize']
         end
       rescue => e
-        @logger.error(e.to_s)
-        @logger.error(e.backtrace)
+        @env.logger.error(e.to_s)
+        @env.logger.error(e.backtrace)
       end
     end
 
