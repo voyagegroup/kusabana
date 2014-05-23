ca72ecd3ea98476dadf6e07f97ed9265d5d906a3
Satoshi Amemiya <satoshi_amemiya@voyagegroup.com>
2014-05-23 15:13:54 +0900

    Remove lines for debug

--- lib/kusabana/http_parser.rb
+++ lib/kusabana/http_parser.rb
@@ -69,16 +69,11 @@ module Kusabana
               @conn.send_data res
             else
               @env.sessions[session_name].merge!(cache_key: cache_key, method: http_method)
-              @conn.relay(session_name, @buffer.gsub(/ \/\S+?#{remote_path} /, " #{remote_path} ").gsub(/\r\n\r\n.+/m, "\r\n\r\n#{body}"))
+              @conn.relay(session_name, @buffer.gsub(/ \/\S+?#{Regexp.escape remote_path} /, " #{remote_path} ").gsub(/\r\n\r\n.+/m, "\r\n\r\n#{body}"))
             end
           end
         else
-          p remote_path
-          p @buffer
           @env.sessions[session_name].merge!(cache_key: cache_key, method: http_method)
-          p @buffer.gsub(/ \/\S+?#{Regexp.escape remote_path} /, " #{remote_path} ")
-          a =  / \/\S+?#{remote_path} /
-          p a
           @conn.relay(session_name, @buffer.gsub(/ \/\S+?#{Regexp.escape remote_path} /, " #{remote_path} "))
         end
       end
